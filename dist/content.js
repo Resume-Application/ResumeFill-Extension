ad396beb540046facbc92b3b5899b7c606800989
arcbhattx <arcbh11@gmail.com>
2026-01-12 00:29:17 -0800

    something

--- dist/content.js
+++ dist/content.js
@@ -1,5 +1,16 @@
 "use strict";
 console.log("ResumeFill content script running");
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOM loaded");
-});
+const firstName = document.getElementById("first_name");
+const lastNameInput = document.getElementById("last_name");
+const emailInput = document.getElementById("email");
+;
+const phoneInput = document.getElementById("phone");
+;
+function setReactInputValue(input, value) {
+    const valueSetter = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, "value")?.set;
+    if (!valueSetter) {
+        throw new Error("Unable to find native input value setter");
+    }
+    valueSetter.call(input, value);
+    input.dispatchEvent(new Event("input", { bubbles: true }));
+}
--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,28 @@
 console.log("ResumeFill content script running");
+
+const firstName = document.getElementById("first_name") as HTMLInputElement | null;
+const lastNameInput = document.getElementById("last_name") as HTMLInputElement | null;
+const emailInput = document.getElementById("email") as HTMLInputElement | null;;
+const phoneInput = document.getElementById("phone") as HTMLInputElement | null;;
+
+function setReactInputValue(
+  input: HTMLInputElement,
+  value: string
+): void {
+  const valueSetter = Object.getOwnPropertyDescriptor(
+    HTMLInputElement.prototype,
+    "value"
+  )?.set;
+
+  if (!valueSetter) {
+    throw new Error("Unable to find native input value setter");
+  }
+
+  valueSetter.call(input, value);
+
+  input.dispatchEvent(
+    new Event("input", { bubbles: true })
+  );
+}
+
+
